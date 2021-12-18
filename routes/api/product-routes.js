577e74a5b228c05c87df2882015176422c950411
Hank Kapka <hankkapka@gmail.com>
2021-12-18 15:02:05 -0600

    get routes for categories and categories by id are working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,6 +15,9 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findByPk(req.params.id).then((productData) => {
+    res.json(productData);
+  });
   // be sure to include its associated Category and Tag data
 });
 
