f18c1613a2da05ba9234a66aa6a542b8e4b7bcf1
Hank Kapka <hankkapka@gmail.com>
2021-12-18 14:23:32 -0600

    added Category and Product models

--- models/Category.js
+++ models/Category.js
@@ -5,16 +5,27 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+    {
+      // define columns
+      category_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category',
+    }
+  );
+  
 
 module.exports = Category;
